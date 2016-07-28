7ea720ba5c2ce2f1d3b8690b1834a08c172e65f3
Amy Laun <amy.laun@kiosk.tm>
2016-07-28 13:55:50 -0700

    Restructure Gulp with Presentation from Summit

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,56 @@
-// Require all the things
-const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      gutil = require('gulp-util'),
-      plumber = require('gulp-plumber'),
-      rename = require('gulp-rename'),
-      minifyCSS = require('gulp-minify-css'),
-      prefixer = require('gulp-autoprefixer'),
-      connect = require('gulp-connect');
-      cp = require('child_process');
+// Toolkit
+var gulp = require('gulp');
+var yargs = require('yargs');
+var cp = require('child_process');
 
-// Set the path variables
-const base_path = './',
-      src = base_path + '_dev/src',
-      dist = base_path + 'assets',
-      paths = {
-          js: src + '/js/*.js',
-          scss: [ '_sass/*.scss',
-                  '_sass/**/* .scss',
-                  '_sass/**/**/*.scss'],
+// Plugins
+var $ = require('gulp-load-plugins')();
+
+// Production Check
+var PRODUCTION = !!(yargs.argv.production);
+
+// Path Variables
+const paths = {
+          scss: [ '_sass/*.scss', '_sass/**/* .scss', '_sass/**/**/*.scss'],
           jekyll: ['index.html', '_includes/*', '_layouts/*', '_projects/*', 'assets/*', 'assets/**/*']
       };
 
 
 // Compile sass to css
 gulp.task('compile-sass', () => {
-  return gulp.src(paths.scss)
-    .pipe(plumber((error) => {
-        gutil.log(gutil.colors.red(error.message));
-        gulp.task('compile-sass').emit('end');
-    }))
-    .pipe(sass())
-    .pipe(prefixer('last 3 versions', 'ie 9'))
-    .pipe(minifyCSS())
-    .pipe(rename({dirname: dist + '/css'}))
-    .pipe(gulp.dest('./'));
+    return gulp.src(paths.scss)
+    .pipe(
+        $.sourcemaps.init()
+    )
+    .pipe(
+        $.sass({
+            includePaths: paths.scss,
+            outputStyle: 'compressed'
+        }).on('error', $.sass.logError)
+    )
+    .pipe(
+        $.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        })
+    )
+    .pipe(
+        $.if(
+            PRODUCTION, $.cssnano()
+        )
+    )
+    .pipe(
+        $.if(
+            !PRODUCTION, $.sourcemaps.write()
+        )
+    )
+    .pipe(
+        $.rename({
+            dirname: './assets/css'
+        })
+    )
+    .pipe(
+        gulp.dest('./')
+    );
 });
 
 // Rebuild Jekyll
@@ -45,7 +62,7 @@ gulp.task('build-jekyll', (code) => {
 
 // Setup Server
 gulp.task('server', () => {
-    connect.server({
+    $.connect.server({
         root: ['_site'],
         port: 4000
     });
@@ -53,8 +70,8 @@ gulp.task('server', () => {
 
 // Watch files
 gulp.task('watch', () => {
-  gulp.watch(paths.scss, ['compile-sass']);
-  gulp.watch(paths.jekyll, ['build-jekyll']);
+    gulp.watch(paths.scss, ['compile-sass']);
+    gulp.watch(paths.jekyll, ['build-jekyll']);
 });
 
 // Start Everything with the default task
